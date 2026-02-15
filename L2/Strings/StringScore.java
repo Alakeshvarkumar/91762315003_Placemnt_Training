f9033bf379f3eb10061136db112d0fe3fe888eb7
Alakesh <alakesh18052006@gmail.com>
2026-02-15 22:11:39 +0530

    Strings

--- L2/Strings/StringScore.java
+++ L2/Strings/StringScore.java
@@ -1,10 +1,13 @@
+
 class StringScore {
+
     public int scoreOfString(String s) {
-        int sum =0;
+        int sum = 0;
         char[] arr = s.toCharArray();
-        for(int i =0;i<arr.length-1;i++){
-            
-            sum+=Math.abs(arr[i]-arr[i+1]);;
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            sum += Math.abs(arr[i] - arr[i + 1]);
+
         }
         return sum;
     }
