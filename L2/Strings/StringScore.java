9f1fd30fb14392249630bea8c5a34bb9f88417af
Alakesh <alakesh18052006@gmail.com>
2026-02-15 21:58:49 +0530

    patterns

---
+++ L2/Strings/StringScore.java
@@ -0,0 +1,11 @@
+class StringScore {
+    public int scoreOfString(String s) {
+        int sum =0;
+        char[] arr = s.toCharArray();
+        for(int i =0;i<arr.length-1;i++){
+            
+            sum+=Math.abs(arr[i]-arr[i+1]);;
+        }
+        return sum;
+    }
+}
