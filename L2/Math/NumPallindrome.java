802ef7274a2db1f1a003e5de5b30f9bae5adeabd
Alakesh <alakesh18052006@gmail.com>
2026-02-17 20:35:05 +0530

    numPallindrome

---
+++ L2/Math/NumPallindrome.java
@@ -0,0 +1,16 @@
+class NumPallindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false; 
+
+        int original = x;
+        int r = 0;
+
+        while (x != 0) {
+            int d = x % 10;
+            r = r * 10 + d;
+            x /= 10;
+        }
+
+        return original == r;
+    }
+}
